da236ca9d8bb783832fca797e2b122d9ea7c646d
norman000 <norman000@mail.ru>
2013-10-21 03:02:54 +0400

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Reverses a string.
+s must be a String type.
---
+++ src/reverse.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ */
+public class reverse {
+    public static void main(String[] args) {
+        String s = "for reverse";
+        String reverse = new StringBuffer(s).reverse().toString();
+        System.out.println(reverse);
+    }
+}
